986713df02233e20023f1d5789bf7e1a51827be5
maiminhtan <71110925+maiminhtan@users.noreply.github.com>
2020-11-15 10:46:48 +0700

    devB_them_sinh_vien

--- models/LopHoc.js
+++ models/LopHoc.js
@@ -2,5 +2,7 @@ var LopHoc = function() {
     this.tenLop = '';
     this.Malop = '';
     this.tenSinhViem = '';
-
+}
+this.themSinhVien = function(sinhVien) {
+    console.log('Thêm Sinh Viên');
 }
