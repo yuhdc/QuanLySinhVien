2a641212251459e7db2a03f228c752fa940c089c
unknown <caoduchuy0147@gmail.com>
2020-11-15 11:02:49 +0700

    dev_huy ver1

--- models/LopHoc.js
+++ models/LopHoc.js
@@ -1,6 +1,17 @@
-var LopHoc = function() {
+var LopHoc = function () {
     this.tenLop = '';
     this.Malop = '';
-    this.tenSinhViem = '';
+    this.tenSinhVien = '';
 
+    this.tenLop = function () {
+        console.log(this.tenLop);
+    }
+
+    this.Malop = function () {
+        console.log(this.Malop);
+    }
+
+    this.tenSinhVien = function () {
+        console.log(this.tenSinhVien);
+    }
 }
